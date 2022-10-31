cdba87fce525e2aaab572d88a1ac4fb5a19e9a24
Goose-kr <Goose-kr>
2022-10-31 13:53:26 +0400

    Initial commit

---
+++ src/main/java/MyStudentHan.java
@@ -0,0 +1,8 @@
+import org.xml.sax.helpers.DefaultHandler;
+
+public class MyStudentHan extends DefaultHandler {
+    boolean average = false;
+    boolean firstname = false;
+    boolean lastname = false;
+    boolean groupnumber = false;
+}
